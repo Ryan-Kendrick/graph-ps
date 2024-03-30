bb527c6da3eec5bbdb3164a34cd62c891ad8c708
root <root@ryan-PC>
2024-03-30 14:41:24 +1300

    replace hardcoded data

--- Add users to groups.ps1
+++ Add users to groups.ps1
@@ -1,71 +1,66 @@
+function Add-UsersToGroups {
+    param (
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Comma separated list of the users to add")]
+        [ValidateNotNullOrEmpty()]
+        $users,
+        [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Comma separated list of the groups to add")]
+        [ValidateNotNullOrEmpty()]
+        $groups
+    )
+
+    $userIds = @()
+    $groupIds = @()
+    Write-Host $users.GetType().Name
+    Write-Host $groups.GetType().Name
+    Write-Host $users
+    Write-Host $groups
+
+    # function Change-StringToArray {
+    #     param (
+    #         $str
+    #     )
+
+    # }
+    # for ($i = 0; $i -lt 2; $i++) {
+    #     $csv = @($users, $groups)[$i]
+    #     $csvToArr = @()
+    #     if ($csv | Select-String -Pattern ",") {
+    #         $csvToArr += $csv -split ','
+    #         $csvToArr = $csvToArr.Trim()
+    #     }
+    # } 
 
 
-# Some code to derive a solution from:
+    foreach ($user in $users) {
+        $thisId = (Get-MgUser -Filter "mail eq '$user'").Id
+        if (($null -ne $thisId) -and ($thisId -is [string])) {
+            $userIds += $thisId
+        } else {
+            Throw "User `"$user`" not found, operation aborted"
+        } 
+    }
 
-# Import a CSV with the list of comps to be added to the group (update <path>)
+    foreach ($group in $groups) {
+        $thisId = (Get-MgGroup -Filter "DisplayName eq '$group'").Id
+        if (($null -ne $thisId) -and ($thisId -is [string])) {
+            $groupIds += $thisId
+        } else {
+            Throw "Group `"$group`" not found, operation aborted"
+        } 
+    }
 
-# $computers = Import-Csv -Path "C:\working\aad-forceDevicesAutoUpdateList1.csv" | Select-Object -ExpandProperty ComputerName
+    Write-Host $userIds
+    Write-Host $groupIds
 
-# $groupName = "testdelete"
+    foreach ($groupId in $GroupIds) {
+        foreach ($userId in $userIds) {
+            New-MgGroupMember -GroupId "$groupId" -DirectoryObjectId "$userId"
+        }
+    }
 
-# #================================================================== # Connect to Microsoft Graph with comp read/write permissions
+    Write-Host "$userIds"
+    Write-Host "$groupIds"
 
-# Connect-MgGraph -Scopes GroupMember.ReadWrite.All, Device.ReadWrite.All
-
-# $group = Get-MgGroup -Filter "DisplayName eq '$groupName'" -ConsistencyLevel eventual
-
-# try {
-
-# foreach ($computer in $computers) { $device = Get-MgDevice -Filter "DisplayName eq '$computer'"
-# Write-host "Adding comp $($comp)to $($group.DisplayName)" -ForegroundColor Green
-
-# # object IT, not the device id
-
-# New-MgGroupMember -GroupId $group.id -DirectoryObjectId $device.id }
-# }
-
-# catch { Write-Host "Error getting information for $computer $_"
-# }
-# ----------
-# $continue = Read-Host "Adding $users to $groups, press y to continue or n to cancel"
-# $groupId = Get-MgGroup -Filter "DisplayName eq 'Leadership Team'"
-
-# get-mggroup -filter "DisplayName eq 'tg-3'"
+}
 
 Connect-MgGraph -NoWelcome -Scopes GroupMember.ReadWrite.All, User.Read.All
-
-$users = "ryan.kendrick93@gmail.com", "Ryan@givecredit.onmicrosoft.com", "RyanK@give-credit.co.nz", "fleeterson@gmail.com"
-$groups = "TG-1", "TG-2", "TG-3", "TG-4"
-$userIds = @()
-$groupIds = @()
-
-foreach ($user in $users) {
-    $thisId = (Get-MgUser -Filter "mail eq '$user'").Id
-    if (($null -ne $thisId) -and ($thisId -is [string])) {
-        $userIds += $thisId
-    } else {
-        Throw "User `"$user`" not found, operation aborted"
-    } 
-}
-
-foreach ($group in $groups) {
-    $thisId = (Get-MgGroup -Filter "DisplayName eq '$group'").Id
-    if (($null -ne $thisId) -and ($thisId -is [string])) {
-        $groupIds += $thisId
-    } else {
-        Throw "Group `"$group`" not found, operation aborted"
-    } 
-}
-
-foreach ($groupId in $GroupIds) {
-    Get-MgGroup -Filter "Id eq '$groupId'"
-    foreach ($userId in $userIds) {
-        New-MgGroupMember -GroupId "$groupId" -DirectoryObjectId "$userId"
-    }
-    Get-MgGroup -Filter "Id eq '$groupId'"
-}
-
-
-
-Write-Host "$userIds"
-Write-Host "$groupIds"
