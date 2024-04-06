d6058e4fa302c872fddc782cfd26726164a97991
root <root@ryan-PC>
2024-04-07 11:45:34 +1200

    Working solution

--- Add guest users.ps1
+++ Add guest users.ps1
@@ -2,19 +2,17 @@ function Add-GuestUsers {
     param (
         [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Comma separated list of names to add. Must be in quotes")]
         [ValidateNotNullOrEmpty()]
-        $name,
+        $displayName,
         [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Comma separated list of email addresses to send guest invitations. Must be in quotes")]
         [ValidateNotNullOrEmpty()]
         $email
     )
-    $paramNames = @("Name", "Email")
-    $params = @($name, $email)
+    $paramNames = @("Display name", "Email address")
+    $params = @($displayName, $email)
 
     for ($i = 0; $i -lt $params.Length; $i++) {
         if ($params[$i] | Select-String -Pattern ",") {
-            Write-host $params[$i]
             $arr = $params[$i] -split ','
-            write-host $arr
             $arr = $arr.Trim()
         } else {
             Throw "Comma separated list not provided"
@@ -23,40 +21,34 @@ function Add-GuestUsers {
     } 
 
     if ($params[0].Length -ne $params[1].Length) {
-        Throw "The length of the list of names and email addresses must be equal"
+        Throw "The length of the list of display names and email addresses must be equal"
     }
 
     $confirmationTable = @()
 
-    for ($i = 0; $i -lt $params.Length; $i++) {
-        $thisParam = $paramNames[$i]
-        for ($j = 0; $j -lt $params[$i][$j].Length; $j++) {
-            $thisUser = [ordered]@{}
-            for ($k = 0; $k -lt $params.Length; $k++) {
-                $thisUser.$thisParam = $params[$k][$j]
-            }
-            $confirmationTable += $thisUser
+    for ($i = 0; $i -lt $params[0].Length; $i++) {
+        $thisUser = [ordered]@{}
+        for ($j = 0; $j -lt $params.Length; $j++) {
+            $thisParam = $paramNames[$j]
+            $thisUser.$thisParam = $params[$j][$i]
         }
+        $confirmationTable += $thisUser
      }
      
-    Write-Host $confirmationTable
-    Write-Host $confirmationTable[0].gettype().name
-    Write-Host $confirmationTable[1]
+   $confirmationTable | Format-Table -AutoSize | Out-Host
 
+   $messageInfo = @{CustomizedMessageBody = "Hello. You are invited to the Contoso organization."}
 
-    # Need confirmation dialogue - array of hash tables > name: [name] email: [email]
+   foreach ($invitee in $confirmationTable) {
+    New-MgInvitation `
+    -InviteRedirectUrl "https://myapps.microsoft.com" `
+    -InvitedUserDisplayName $invitee["Display name"] `
+    -InvitedUserEmailAddress $invitee["Email address"] `
+    -InvitedUserMessageInfo $messageInfo `
+    -SendInvitationMessage
+   }
 }
 
-# $invitations = import-csv c:\bulkinvite\invitations.csv
+Connect-MgGraph -Scopes 'User.ReadWrite.All'
 
-# $messageInfo = [Microsoft.Graph.PowerShell.Models.MicrosoftGraphInvitation]@{ `
-#    CustomizedMessageBody = "Hello. You are invited to the Contoso organization." }
-
-# foreach ($email in $invitations)
-#    {New-MgInvitation `
-#       -InviteRedirectUrl https://myapps.microsoft.com ` 
-#       -InvitedUserDisplayName $email.Name `
-#       -InvitedUserEmailAddress $email.InvitedUserEmailAddress `
-#       -InvitedUserMessageInfo $messageInfo `
-#       -SendInvitationMessage 
-#    }
+# Add confirmation prompt
