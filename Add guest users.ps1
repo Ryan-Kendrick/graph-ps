8f299a71a4cfc5e9c14ecdec5d65efb0f00575f9
root <root@ryan-PC>
2024-05-04 15:49:26 +1200

    working input validation

--- Add guest users.ps1
+++ Add guest users.ps1
@@ -3,43 +3,48 @@ function Add-GuestUsers {
         [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Comma separated list of names to add. Must be in quotes")]
         [ValidateNotNullOrEmpty()]
         $displayName,
+
         [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Comma separated list of email addresses to send guest invitations to. Must be in quotes")]
         [ValidateNotNullOrEmpty()]
         $email
     )
+
     $paramNames = @("Display name", "Email address")
     $params = @($displayName, $email)
 
-    for ($i = 0; $i -lt $params.Length; $i++) { # Separate each user property into an array, each element representing a different user
-        if (($params[$i] | Select-String -Pattern ",") -and ($null -ne $params[$i][$params[$i].IndexOf(",")+1])) {
-            $arr = $params[$i] -split ','
-            $arr = $arr.Trim()
-            if ($i -eq 0) {
-                foreach ($namePair in $arr) {
-                    if  (!$namePair.IndexOf(' ')+1) {
-                        Throw "$namePair is not a valid name"
-                    }
-                }
-            }
+    # Validate and split comma-separated lists
+    for ($i = 0; $i -lt $params.Length; $i++) { 
+        if ($params[$i] -match ",") {
+            $arr = $params[$i] -split ',' | ForEach-Object { $_.Trim() }
+            $params[$i] = $arr
         } else {
-            Throw "A required comma separated list was not provided"
+            Throw "A required comma-separated list was not provided for $($paramNames[$i])."
         }
-        $params[$i] = $arr ? $arr : $params[$i]
     } 
 
-    if ($params[0].Length -ne $params[1].Length) { #Validate email addresses
-        Throw "The length of the list of display names and email addresses must be equal"
+    # Validate display names
+    foreach ($namePair in $params[0]) { 
+        $spacePos = $namePair.IndexOf(' ') 
+        if  (!(($spacePos -ge 0) -and ($spacePos -lt ($namePair.length -1)))) {
+            Throw "$namePair is not a full name"
+        }
     }
-    foreach ($email in $params['email']) {
-        if ($email -match "[a-z0-9\._%+!$&*=^|~#%'`?{}\/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,16})\") {
+
+    # Validate email addresses
+    foreach ($email in $params[1]) { 
+        if ($email -notmatch "^[A-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\.[A-Z0-9_!#$%&'*+/=?`{|}~^-]+↵)*@[A-Z0-9-]+(?:\.[A-Z0-9-]+)*$"
+        ) {
             Throw "$email is not a valid email address"
         }
     }
+    
+    # Check that the lengths match
+    if ($params[0].Length -ne $params[1].Length) { 
+        Throw "The number of display names and email addresses must be equal."
+    }
 
-    $ar1, $ar2 = $params 
-
+    # Display a table of the guest user accounts to be created
     $confirmationTable = @()
-
     for ($i = 0; $i -lt $params[0].Length; $i++) {
         $thisUser = [ordered]@{}
         for ($j = 0; $j -lt $params.Length; $j++) {
@@ -48,7 +53,8 @@ function Add-GuestUsers {
         }
         $confirmationTable += $thisUser
     }
-        
+
+    # Prompt user for confirmation
     $confirmationTable | Format-Table -AutoSize | Out-Host
     $proceed = Read-Host "Enter 'y' to invite these users"
 
@@ -56,7 +62,7 @@ function Add-GuestUsers {
         $messageInfo = @{CustomizedMessageBody = "Hello. You are invited to the Contoso organization."}
         foreach ($invitee in $confirmationTable) {
             Write-Host "$($invitee["Display name"][1])"
-            Write-host "Inviting $($invitee["Display name"])"
+            Write-host "Sending invitation for $($invitee["Display name"]) to $($invitee["Email address"])"
             New-MgInvitation `
             -InviteRedirectUrl "https://myapps.microsoft.com" `
             -InvitedUserDisplayName $invitee["Display name"] `
@@ -71,6 +77,4 @@ function Add-GuestUsers {
 
 Connect-MgGraph -Scopes 'User.ReadWrite.All'
 
-# fix regex
-# fix multiple names check
 # add params for jobtitle, department, companyname, manager
