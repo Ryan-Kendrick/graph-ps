7e7c4126c4af6e4b9c9138f79c22fcda0fcd603f
root <root@ryan-PC>
2024-05-06 06:44:27 +1200

    touch up comments

--- Add guest users.ps1
+++ Add guest users.ps1
@@ -21,7 +21,7 @@ function Add-GuestUsers {
             Throw "A required comma-separated list was not provided for $($paramNames[$i])."
         }
     } 
-
+    
     # Validate display names
     foreach ($namePair in $params[0]) { 
         $spacePos = $namePair.IndexOf(' ') 
@@ -29,7 +29,9 @@ function Add-GuestUsers {
             Throw "$namePair is not a full name"
         }
     }
-
+    
+    
+    
     # Validate email addresses
     foreach ($email in $params[1]) { 
         if ($email -notmatch "^[A-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\.[A-Z0-9_!#$%&'*+/=?`{|}~^-]+↵)*@[A-Z0-9-]+(?:\.[A-Z0-9-]+)*$"
@@ -42,8 +44,28 @@ function Add-GuestUsers {
     if ($params[0].Length -ne $params[1].Length) { 
         Throw "The number of display names and email addresses must be equal."
     }
+    
+    
+    # Prompt user for guest properties
+    Write-Host "Enter the required properties for a new guest user"
+    $jobTitle = Read-Host "Job title"
+    $companyName = Read-Host "Company name"
+    $department = Read-Host "Department"
+    $managerEmail = Read-Host "Manager email address"
 
-    # Display a table of the guest user accounts to be created
+    $managerId = Get-MgUser -Filter "mail eq '$managerEmail'" | Select-Object -ExpandProperty Id
+    if ($null -eq $managerId) {
+        Throw "Manager not found for $managerEmail"
+    }
+    
+    $props = @{
+        jobTitle = $jobTitle
+        companyName = $companyName
+        department = $department
+        usageLocation = 'NZ'
+    }
+
+    # Construct a table of the guest user accounts to be created
     $confirmationTable = @()
     for ($i = 0; $i -lt $params[0].Length; $i++) {
         $thisUser = [ordered]@{}
@@ -54,27 +76,9 @@ function Add-GuestUsers {
         $confirmationTable += $thisUser
     }
     
-    # Prompt user for confirmation
+    # Display table and prompt user for confirmation
     $confirmationTable | Format-Table -AutoSize | Out-Host
     $proceed = Read-Host "Enter 'y' to invite these users"
-    
-    # Prompt user for properties
-    Write-Host "Enter the required properties for a new guest user"
-    $jobTitle = Read-Host "Job title"
-    $companyName = Read-Host "Company name"
-    $department = Read-Host "Department"
-    $managerEmail = Read-Host "Manager email address"
-    $managerId = Get-MgUser -ErrorAction Stop -Filter "mail eq '$managerEmail'" | Select-Object -ExpandProperty Id
-    if ($null -eq $managerId) {
-        Throw "Manager not found for $managerEmail"
-    }
-
-    $props = @{
-        jobTitle = $jobTitle
-        companyName = $companyName
-        department = $department
-        usageLocation = 'NZ'
-    }
     
     # Create users
     if ($proceed -match '(?i)y') {
@@ -90,10 +94,12 @@ function Add-GuestUsers {
             }
             $newGuest = New-MgInvitation -BodyParameter $invBody
             Update-MgUser -UserId $newGuest.InvitedUser.Id -BodyParameter $props
-            Set-MgUserManagerByRef -UserId $newGuest.InvitedUser.Id -BodyParameter @{'@odata.id' = "https://graph.microsoft.com/v1.0/users/$managerId"}
+            Set-MgUserManagerByRef -UserId $newGuest.InvitedUser.Id -BodyParameter @{'@odata.id' = "https://graph.microsoft.com/v1.0/users/$managerId"
         }
+        }
+        Write-Host "Guest accounts created. Confirm the properties are as expected in the Azure portal."
     } else {
-            Write-Host "Operation aborted by user"
+        Write-Host "Operation aborted by user"
     }
 }
 
