dd52c33c21645ecb4879318e5528232e82f6975f
root <root@ryan-PC>
2024-05-20 15:39:24 +1200

    describe new value on printout

--- Blame SD.ps1
+++ Blame SD.ps1
@@ -8,22 +8,30 @@ function Blame-SD {
         $userId = (Get-MgUser -Filter  "mail eq '$userEmail'").id
         $userAuditLog = Get-MgAuditLogDirectoryAudit -Filter "(category eq 'GroupManagement' or category eq 'UserManagement') and (targetResources/any(t:t/id eq '$userId'))" 
 
-     
+        $debug = $userAuditLog.TargetResources[3].ModifiedProperties
+        $debug | ft *
+
         $userAuditLog | Select-Object @{Name="Time"; Expression={$_.ActivityDateTime}}, `
         @{Name="Action"; Expression={$_.ActivityDisplayName}}, `
-        @{Name="Modified"; Expression={
+        @{Name="Changed"; Expression={
             try {
-                $path = $_.TargetResources[0].ModifiedProperties.NewValue[1].trim("`"")
-                return $path
+                $path = $_.TargetResources[0].ModifiedProperties.NewValue[1]
+                return $path.trim("`"")
             } catch {
             }
-            $path = $_.TargetResources[0].ModifiedProperties.OldValue[1].trim("`"")
-            return $path
+            $path = $_.TargetResources[0].ModifiedProperties.OldValue[1]
+            return $path.trim("`"")
             }}, `
-        @{Name="Target"; Expression={$_.TargetResources[0] | Select-Object -ExpandProperty UserPrincipalName}}, `
-        @{Name="Admin"; Expression={$_.InitiatedBy.User.UserPrincipalName}} | Format-Table -AutoSize
+        @{Name='New Value'; Expression={
+            try {
+                $path = $_.TargetResources[0].ModifiedProperties.NewValue[0]
+                return $path.trim("`"", "[", "]")
+            } catch {
+            }
+        }}, `
+        @{Name="Target"; Expression={$upn = $_.TargetResources[0] | Select-Object -ExpandProperty UserPrincipalName 
+            return $upn -replace '#EXT#.*', ''}}, `
+        @{Name="Admin"; Expression={$_.InitiatedBy.User.UserPrincipalName.TrimStart("#EXT#")}} | Format-Table -AutoSize
     }
 
 Connect-MgGraph -Scopes AuditLog.Read.All
-
-# Conditionally add more information for UM actions
