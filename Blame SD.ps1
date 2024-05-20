07d10f88f04265c75e7ae1000f4eca4e5f2917c0
root <root@ryan-PC>
2024-05-21 10:30:26 +1200

    inputobject WIP

--- Blame SD.ps1
+++ Blame SD.ps1
@@ -2,11 +2,26 @@ function Blame-SD {
     param(
         [Parameter(Mandatory = $true, HelpMessage = "Email address of the user to fetch audit logs for. Must be in quotes")]
         [ValidateNotNullOrEmpty()]
-        $userEmail
+        [string]$userEmail,
+        [Parameter(Mandatory = $false, HelpMessage = "Increase the search range from 7 days to 30 days")]
+        [switch]$long
         )
-
+        
         $userId = (Get-MgUser -Filter  "mail eq '$userEmail'").id
-        $userAuditLog = Get-MgAuditLogDirectoryAudit -Filter "(category eq 'GroupManagement' or category eq 'UserManagement') and (targetResources/any(t:t/id eq '$userId'))" 
+        $dateRange = if (!$long.IsPresent) {
+            return 7
+        } else {
+            return 30
+        }
+        $auditRange = (Get-Date).AddDays($dateRange)
+        write-host $auditRange
+
+        $searchParams = @{
+            Filter="(category eq 'GroupManagement' or category eq 'UserManagement') and (targetResources/any(t:t/id eq '$userId'))"
+            EndDateTime = $auditRange
+        }
+
+        $userAuditLog = Get-MgAuditLogDirectoryAudit -InputObject $searchParams
 
         $userAuditLog | Select-Object @{Name="Time"; Expression={$_.ActivityDateTime}}, `
         @{Name="Action"; Expression={$_.ActivityDisplayName}}, `
@@ -28,7 +43,7 @@ function Blame-SD {
         }}, `
         @{Name="Target"; Expression={$upn = $_.TargetResources[0] | Select-Object -ExpandProperty UserPrincipalName 
             return $upn -replace '#EXT#.*', ''}}, `
-        @{Name="Admin"; Expression={$_.InitiatedBy.User.UserPrincipalName.TrimStart("#EXT#")}} | Format-Table -AutoSize
+        @{Name="Admin"; Expression={$_.InitiatedBy.User.UserPrincipalName.trim("`"", "[", "]")}} | Format-Table -AutoSize
     }
 
 Connect-MgGraph -Scopes AuditLog.Read.All
