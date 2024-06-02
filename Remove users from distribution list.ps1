0d42a0c2703395b6229a6d7ab28bd886b362f14b
root <root@ryan-PC>
2024-06-03 10:09:09 +1200

    initial commit of remove users from DL

---
+++ Remove users from distribution list.ps1
@@ -0,0 +1,29 @@
+function Remove-UsersFromDL {
+    param (
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Comma separated list of user email addresses to remove from distribution list. Must be in quotes")]
+        [ValidateNotNullOrEmpty()]
+        [string]$userEmails,
+
+        [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Single distribution list to remove users from. Must be in quotes")]
+        [ValidateNotNullOrEmpty()]
+        [string]$distributionList
+    )
+
+    $userArr = ($userEmails -split ',').Trim()
+
+    # Remove each user from the distribution list
+    foreach ($user in $userArr) {
+        try {
+            Remove-DistributionGroupMember -Identity $distributionList -Member $user -BypassSecurityGroupManagerCheck
+        } catch {
+           Write-Host "Failed to add $user to $distributionList"
+        }
+    }
+
+    # Prompt user to display all distribution list members
+    Write-Host "Enter 'y' to display all members of $distributionList"
+    $displayDL = Read-Host "Continue"
+    if ($displayDL -eq "(?i)y") {
+        Get-DistributionGroupMember -Identity $distributionList | Select-Object DisplayName, PrimarySmtpAddress, RecipientType
+    } 
+}
