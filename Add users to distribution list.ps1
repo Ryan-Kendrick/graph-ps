553a579ea31a1ba78bbbd4a36a6443dfc8d98b80
root <root@ryan-PC>
2024-06-03 09:58:56 +1200

    initial commit of add users to DL

---
+++ Add users to distribution list.ps1
@@ -0,0 +1,29 @@
+function Add-UsersToDL {
+    param (
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Comma separated list of user email addresses to add to mailbox. Must be in quotes")]
+        [ValidateNotNullOrEmpty()]
+        [string]$userEmails,
+
+        [Parameter(Mandatory = $true, Position = 2, HelpMessage = "Single shared mailbox to add users to. Must be in quotes")]
+        [ValidateNotNullOrEmpty()]
+        [string]$distributionList
+    )
+
+    $userArr = ($userEmails -split ',').Trim()
+
+    # Add each user to the shared mailbox
+    foreach ($user in $userArr) {
+        try {
+            Add-DistributionGroupMember -Identity $distributionList -Member $user -BypassSecurityGroupManagerCheck
+        } catch {
+           Write-Host "Failed to add $user to $distributionList"
+        }
+    }
+
+    # Prompt user to display all distribution list members
+    Write-Host "Enter 'y' to display all members of $distributionList"
+    $displayDL = Read-Host "Continue"
+    if ($displayDL -eq "(?i)y") {
+        Get-DistributionGroupMember -Identity $distributionList -ResultSize | Select-Object DisplayName, PrimarySmtpAddress, RecipientType
+    } 
+}
