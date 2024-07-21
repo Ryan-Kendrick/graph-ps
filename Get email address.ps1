d95061bffff553258fe5a65d4c49b3d2cc24e60d
Ryan Kendrick <ryan.kendrick93@gmail.com>
2024-07-21 13:01:31 +1200

    array of objects display solution

--- Get email address.ps1
+++ Get email address.ps1
@@ -7,21 +7,41 @@ function Get-EmailAddress {
     $namesArr = ($names -split ',').Trim()
 
     $emailsArr = @()
-    $multipleEmailsArr = @()
-    $notFoundArr = @()
-
+    $ambiguousResults = $false
 
     foreach ($name in $namesArr) {
+
+      # Fetch the email address property for each display name provided
       $userEmail = (Get-MgUser -Filter  "displayname eq '$name'").mail
-      if ($userEmail.GetType().name -eq "Object[]") {
-        $multipleEmailsArr += $name
-      } elseif ($userEmail.GetType().name -eq "String") {
-        $emailsArr += $userEmail
+
+      if ($userEmail) {
+
+        # Add the email address property to the results array as appropriate for one, multiple, or no matches
+        if ($userEmail.GetType().name -eq "Object[]") {
+          $ambiguousResults = $true
+          $emailsArr += [PSCustomObject][Ordered]@{
+            DisplayName = $name
+            EmailAddress = $userEmail -join ', '
+          }
+        } elseif ($userEmail.GetType().name -eq "String") {
+          $emailsArr += [PSCustomObject][Ordered]@{
+            DisplayName = $name
+            EmailAddress = $userEmail
+          }
+        }
       } else {
-        $notFoundArr += $name
+      $emailsArr += [PSCustomObject][Ordered]@{
+        DisplayName = $name
+        EmailAddress = 'not found'
+        }
       }
+      
     }
 
-    Write-Host $emailsArr
+    # Display the results array of objects as a table
+    $emailsArr | Format-Table
+    if ($ambiguousResults) {
+      Write-Host "Multiple potential matches found for some names. Verify which account is correct before proceeding."
+    }
 
 }
